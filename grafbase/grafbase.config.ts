115abe018a3c3e3c4ae0a9f8374803f960d864a7
Premium Design Partners <coreyshirey@premiumdesignpartners.com>
2023-06-26 14:36:10 -0400

    Update grafbase.config.ts

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -4,7 +4,7 @@ const openai = connector.OpenAPI({
   schema:
     'https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml',
   headers: (headers) => {
-    headers.static('Authorization', `Bearer ${g.env('OPENAI_API_KEY')}`)
+    headers.static('Authorization', `Bearer ${g.env('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')}`)
   },
   transforms: { queryNaming: 'OPERATION_ID' }
 })
